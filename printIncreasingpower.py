811db44c63938e07ba854e77a5b2ece3e623ed14
JPThrives <boruahjp1999@gmail.com>
2024-12-30 12:21:47 +0530

    More Practice

---
+++ printIncreasingpower.py
@@ -0,0 +1,10 @@
+def printIncreasingpower(x):
+    power = 1
+    square = 0
+    while(1):
+        square = power * power
+        if square > x:
+            break
+        print(square,end = " ")
+        power += 1
+printIncreasingpower(30)
